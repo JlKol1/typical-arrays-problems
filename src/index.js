3820888f2220a4870858617ae8622b6aa14632cb
JlKol1 <yulyak2603@gmail.com>
2021-03-01 20:00:37 +0300

    task js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == null) {  
+    return 0
+  } else if (array.length == 0) {
+    return 0
+  } else {
+  return array.sort((a,b)=> a - b)[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == null) {  
+    return 0
+  } else if (array.length == 0) {
+    return 0
+  } else {
+  return array.sort((a,b) => b - a)[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == null) {  
+    return 0
+  } else if (array.length == 0) {
+    return 0
+  } else {
+    return array.reduce((sum, el)=>sum + el)/array.length;
+   }
 }
